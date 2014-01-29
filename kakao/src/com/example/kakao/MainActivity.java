4857e6adb8d7a0a6eaf65090a9add3776a633068
stu42 <stu42@TEACHER>
2014-01-29 09:19:07 +0900

    Add room activity.

--- kakao/src/com/example/kakao/MainActivity.java
+++ kakao/src/com/example/kakao/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.kakao;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private View mFriend;
+    private LinearLayout mFriend;
 	private View mChat;
 	private View mSearch;
 	private View mMore;
@@ -18,7 +22,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFriend = findViewById(R.id.friend);
+        
+        mFriend = (LinearLayout) findViewById(R.id.friend);
         mChat = findViewById(R.id.chat);
         mSearch = findViewById(R.id.search);
         mMore = findViewById(R.id.more);
@@ -43,10 +48,52 @@ public class MainActivity extends Activity {
 				mMore.setVisibility(View.GONE);
 			}
 		});
+        
+        setPerson();
     }
 
+	private String names[] = new String[] {
+		"Seungwon", "Jongbae", "Wooseok", "Jinwoo"
+	};
+	
+	private String texts[] = new String[] {
+			"Hi", "Hello", "Nice to meet you", "Me too."
+	};
+	
+	private Integer pics[] = new Integer[] {
+			R.drawable.profile, R.drawable.profile2, 
+			R.drawable.profile, R.drawable.profile2	
+	};
+	
+	private TextView mName;
+	private ImageView mPic;
+	private TextView mText;
+	
+    private void setPerson() {
+    	mFriend = (LinearLayout) findViewById(R.id.friend);
+    	for (int i = 0; i < 4; ++i) {
+	    	View friendItemView = View.inflate(this, R.layout.friend_item, null);
+			mFriend.addView(friendItemView);
+	    	mName = (TextView) friendItemView.findViewById(R.id.name);
+	    	mName.setText(names[i]);
+	    	mPic = (ImageView) friendItemView.findViewById(R.id.pic);
+	    	mPic.setImageResource(pics[i]);
+	    	mText = (TextView) friendItemView.findViewById(R.id.text);
+	    	final String roomName = names[i];
+	    	friendItemView.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+					intent.putExtra("name", roomName);
+					startActivity(intent);
+				}
+			});
+	    	mText.setText(texts[i]);
+    	}
+	}
 
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
---
+++ kakao/src/com/example/kakao/RoomActivity.java
@@ -0,0 +1,54 @@
+package com.example.kakao;
+
+import java.util.Date;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.util.TimeUtils;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class RoomActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+        String name = getIntent().getExtras().getString("name");
+        setTitle(name);
+        
+        initSendButton();
+    }
+
+	private void initSendButton() {
+		View send = findViewById(R.id.send);
+		send.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText text = (EditText) findViewById(R.id.edit);
+				String message = text.getEditableText().toString();
+				addMessageItem(message);
+				text.setText("");
+			}
+		});
+	}
+
+	private void addMessageItem(String messageString) {
+		View item = View.inflate(this, R.layout.message_item, null);
+		TextView message = (TextView) item.findViewById(R.id.message);
+		TextView time = (TextView) item.findViewById(R.id.time);
+		time.setText(DateFormat.format("hh:mm", new Date()));
+		
+		message.setText(messageString);
+		LinearLayout room = (LinearLayout) findViewById(R.id.room);
+		room.addView(item, room.getChildCount() - 1);
+		
+	}
+}
