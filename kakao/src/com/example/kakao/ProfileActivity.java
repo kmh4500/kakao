f6521f91c09a66f28f8af7edf7b2c0ece809384c
stu42 <stu42@TEACHER>
2014-02-05 13:54:11 +0900

    Show name in profile page.

--- kakao/src/com/example/kakao/ProfileActivity.java
+++ kakao/src/com/example/kakao/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.kakao;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class ProfileActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
+		String name = getIntent().getExtras().getString("name");
+		TextView view = (TextView) findViewById(R.id.profile_name);
+		view.setText(name);
 	}
 }
