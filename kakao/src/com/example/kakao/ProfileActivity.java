bdedead4ee4d3c8ecd92a443bffeac0c335dca00
stu42 <stu42@TEACHER>
2014-02-05 14:05:37 +0900

    Close profile activity x button.

--- kakao/src/com/example/kakao/ProfileActivity.java
+++ kakao/src/com/example/kakao/ProfileActivity.java
@@ -2,6 +2,9 @@ package com.example.kakao;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
@@ -13,5 +16,14 @@ public class ProfileActivity extends Activity {
 		String name = getIntent().getExtras().getString("name");
 		TextView view = (TextView) findViewById(R.id.profile_name);
 		view.setText(name);
+		
+		ImageView close = (ImageView) findViewById(R.id.close);
+		close.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 }
